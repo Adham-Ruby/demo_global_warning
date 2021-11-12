f38ef0e184cf5501db5dca243b1002739c0c574a
Adham-Ruby <84845508+Adham-Ruby@users.noreply.github.com>
2021-11-12 17:14:20 +0200

    Created a demo

---
+++ js/script.js
@@ -0,0 +1,232 @@
+// upper navbar bg effect, // back to top button JS
+
+let body = document.body,
+html = document.documentElement,
+scrollPos,
+upperNav = document.querySelector('.upper-nav'),
+mainHeading = document.querySelector('#main-heading'),
+ulMenu = document.querySelector('#ul-menu'),
+logo = document.querySelector('.logo');
+
+let pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight),
+offset = 400,
+backToTopBtn = document.querySelector('.back-to-top-btn');
+
+let formIcon = document.querySelector('.form-icon'),
+clickIcon = 0,
+formContainer = document.querySelector('.form-container'),
+closeForm = document.querySelector('.close-form-icon');
+
+if (pageHeight != undefined) {
+    offset = pageHeight / 2.5;
+} 
+
+let scrollPadd_view = window.matchMedia("(max-width: 950px)");
+
+window.addEventListener('scroll', (event) => {
+    // navbar effect
+    scrollPos = body.scrollTop || html.scrollTop;
+    if (scrollPos > 0) {
+        upperNav.classList.add('background-scroll'); 
+        mainHeading.classList.add('text-gradient-scroll'); 
+        if (scrollPadd_view.matches) {
+                        
+        } else {
+            ulMenu.classList.add('padding-x-smaller');            
+        }
+        logo.classList.add('margin-left-smaller'); 
+    } else {
+        upperNav.classList.remove('background-scroll');
+        mainHeading.classList.remove('text-gradient-scroll');
+        ulMenu.classList.remove('padding-x-smaller'); 
+        logo.classList.remove('margin-left-smaller');  
+    }
+
+    if(scrollPos > offset) {
+        formIcon.classList.add('scroll-move');
+        formContainer.classList.add('scroll-move');
+        backToTopBtn.classList.add('scroll-top');
+    } else {
+        formIcon.classList.remove('scroll-move');
+        formContainer.classList.remove('scroll-move');
+        backToTopBtn.classList.remove('scroll-top');
+    }
+});
+
+//  click audio effect
+
+let clickAudio = new Audio('./audio/click.wav'),
+exploreBtn = document.querySelector('#explore-button');
+
+exploreBtn.addEventListener('click', () => {
+    clickAudio.play();
+})
+
+// text on video appear up effect
+
+let videoHeading = document.querySelector('.video-heading'),
+videoText = document.querySelector('.video-text'),
+videoImg = document.querySelector('.video-img'),
+videoHr = document.querySelector('#video-hr');
+
+window.addEventListener('load', (event) => {
+    videoHeading.classList.add('up');
+    videoImg.classList.add('up');
+    videoText.classList.add('up');
+    exploreBtn.classList.add('up');
+    videoHr.classList.add('up');
+});
+
+// intersection point
+
+const sliders = document.querySelectorAll('.slide-in'),
+appearOptions = {
+    threshold : 0.4
+};
+
+const appearOnScrollObserver = new IntersectionObserver(function(entries, appearOnScrollObserver){
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('appear');
+            appearOnScrollObserver.unobserve(entry.target);
+        } 
+    });
+}, appearOptions);
+
+sliders.forEach(slide => {
+    appearOnScrollObserver.observe(slide);
+});
+
+// carousel functionality
+
+let leftArr = document.querySelector('#left-arr'),
+rightArr = document.querySelector('#right-arr'),
+carouselCards = document.querySelectorAll('.carousel-card');
+
+let l = 0,
+movePer,
+maxMove;
+
+let mobile_view = window.matchMedia("(max-width: 530px)"),
+tablet_view = window.matchMedia("(max-width: 930px)");
+
+function matchingMedia() {
+    if (mobile_view.matches) {
+        maxMove = (carouselCards.length - 1) * movePer;
+    } else if (tablet_view.matches) {
+        maxMove = (carouselCards.length - 2) * movePer;
+    } else {
+        maxMove = (carouselCards.length - 3) * movePer;
+    }
+}
+
+function moveMeasuring() {
+    let marginX = parseFloat(getComputedStyle(carouselCards[0]).marginRight) * 2;
+    movePer = carouselCards[0].offsetWidth + marginX;
+    matchingMedia();
+}
+
+rightArr.addEventListener('click', () => {
+    moveMeasuring();
+    l = l + movePer;
+        // if (carouselCards == 1){l = 0;}
+        if (l > 0) {leftArr.classList.remove('disabled')};
+        if (l == maxMove) {rightArr.classList.add('disabled')};
+		for(const i of carouselCards)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.transform = 'translateX(' + -l +'px)'; // '-' + l + '%';
+            // i.style.left = '-' + l + 'px';
+		}
+});
+
+leftArr.addEventListener("click", () => {
+    moveMeasuring();
+    l = l - movePer;
+		if (l <= 0){l = 0;}
+        if (l == 0) {leftArr.classList.add('disabled')};
+        if (l < maxMove) {rightArr.classList.remove('disabled')};
+		for(const i of carouselCards){
+				i.style.transform = 'translateX(' + -l + 'px)';  // '-' + l + '%';
+		}
+});
+
+// progress bar scroll JS
+
+const containerJs = document.getElementById('js-container');
+const highlight = document.getElementById('js-highlight');
+var containerHeight;
+
+window.onscroll = function() {
+    
+  containerHeight = containerJs.offsetHeight - window.innerHeight;
+  containerPos = containerJs.getBoundingClientRect();
+  diff = containerHeight + containerPos.top;
+  progressPercentage = diff / containerHeight * 100;
+  cssWidth = Math.floor(100 - progressPercentage);
+  highlight.style.width = cssWidth + "%";
+}
+
+// contact form 
+
+formIcon.addEventListener('click', () => {
+    clickIcon ++;
+    if (clickIcon % 2 == 0) {
+        formContainer.classList.remove('open');
+    } else {
+        formContainer.classList.add('open');
+    }
+});
+
+closeForm.addEventListener('click', () => {
+    clickIcon ++;
+    formContainer.classList.remove('open');
+});
+
+// side nav bar on click drop down menu appear
+
+let globalDropDowns = document.querySelectorAll('.global-dropmenu'),
+dropItems = document.querySelectorAll('.dropdown-items'),
+arrNavDowns = document.querySelectorAll('.arr-nav-down');
+
+globalDropDowns.forEach((item, index) => {
+    item.onclick = () => {
+        dropItems[index].classList.toggle('show-drop');
+        arrNavDowns[index].classList.toggle('rotate-top'); 
+    }
+});
+
+// burger clicked
+
+let menuNav = document.querySelector('.menu-nav'),
+burgerIcon = document.querySelector('.menu-btn'),
+closeNavIcon = document.querySelector('.close-nav-icon'),
+burgerIconX = document.querySelector('.menu-btn-burger'),
+clickBurgerIcon = 0;
+
+burgerIcon.onclick = () => {
+    clickBurgerIcon ++;
+    if (clickBurgerIcon % 2 == 0) {
+        menuNav.classList.remove('open');
+        burgerIconX.classList.remove('openX');        
+    } else {
+    menuNav.classList.add('open');
+    burgerIconX.classList.add('openX');
+    }    
+}
+
+closeNavIcon.onclick = () => {
+    clickBurgerIcon ++;
+    menuNav.classList.remove('open');
+    burgerIconX.classList.remove('openX');
+}
+
+let navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.onclick = () => {
+        clickBurgerIcon++;
+        menuNav.classList.remove('open');
+        burgerIconX.classList.remove('openX');
+    }
+});
